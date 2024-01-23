2a8001a43d24fd031e9248b78f7d466bb27e4225
Wesley Ramos <dev.wesleygabriel@gmail.com>
2024-01-23 03:48:00 -0300

    feat: adding database service

---
+++ src/services/DatabaseService.ts
@@ -0,0 +1,34 @@
+import config from '../../data/mysql.json';
+import mysql, {Connection} from 'mysql2/promise';
+
+class DatabaseService {
+
+    client: Connection;
+
+    async start() {
+        console.log('[Simple Ticket] [Database] [INFO]: iniciando conexão ao banco de dados...');
+
+        this.client = await mysql.createConnection({
+            enableKeepAlive: true,
+            host: config.MYSQL_HOSTNAME,
+            user: config.MYSQL_USERNAME,
+            password: config.MYSQL_PASSWORD,
+            database: config.MYSQL_DATABASE
+        });
+
+        await this.client.query(`
+            CREATE TABLE IF NOT EXISTS tickets
+            (
+                id        VARCHAR(36) NOT NULL,
+                role_id   BIGINT(18)  NOT NULL,
+                user_id   BIGINT(18)  NOT NULL,
+                thread_id BIGINT(18)  NOT NULL,
+                PRIMARY KEY (id)
+            );
+        `);
+
+        console.log('[Simple Ticket] [Database] [INFO]: a conexão com o banco de dados foi aberta.');
+    }
+}
+
+export default new DatabaseService();
