3406de3b46a84cee0e91e39cc8143359d399eb97
Wesley Ramos <dev.wesleygabriel@gmail.com>
2024-01-24 01:16:00 -0300

    feat: adding sector controller

---
+++ src/controllers/SectorController.ts
@@ -0,0 +1,39 @@
+import Sector from "../models/Sector";
+import Sectors from "../../data/sector.json";
+
+class SectorController {
+
+    cache: Map<String, Sector>;
+
+    constructor() {
+        this.cache = new Map();
+    }
+
+    async start() {
+        console.log('[Simple Ticket] [Sector] [INFO]: o controlador de setores foi inicializado.');
+
+        for (const sector of Sectors) {
+            await this.create(new Sector(sector.id, sector.label));
+        }
+
+        console.log('[Simple Ticket] [Sector] [INFO]: os setores foram carregados com êxito.');
+    }
+
+    async create(category: Sector) {
+        this.cache.set(category.id, category);
+    }
+
+    async remove(category: Sector) {
+        this.cache.delete(category.id);
+    }
+
+    getCategoryById(id: string) {
+        return Array.from(this.cache.values()).find(category => category.id === id);
+    }
+
+    getCategoryByLabel(label: string) {
+        return Array.from(this.cache.values()).find(category => category.label === label);
+    }
+}
+
+export default new SectorController();
