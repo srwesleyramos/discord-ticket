3883352bc489251b137c23be4b7c316b87abc741
Wesley Ramos <dev.wesleygabriel@gmail.com>
2024-01-25 01:04:00 -0300

    feat: adding ticket row dto

---
+++ src/dto/TicketDTO.ts
@@ -0,0 +1,8 @@
+import {RowDataPacket} from "mysql2";
+
+export default interface TicketDTO extends RowDataPacket {
+    id: string,
+    sector_id: string,
+    thread_id: string
+    user_id: string,
+}
