17dbb6255722b95aef315d94cbaa6b975d7b7da9
Wesley Ramos <dev.wesleygabriel@gmail.com>
2024-01-28 17:10:00 -0300

    feat: adding getSlashCommand

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,4 +1,4 @@
-import {ChatInputCommandInteraction, Client} from "discord.js";
+import {ChatInputCommandInteraction, Client, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder} from "discord.js";
 
 export default class Command {
 
@@ -12,5 +12,9 @@ export default class Command {
 
     async execute(interaction: ChatInputCommandInteraction) {
         console.log('o comando', this.name, 'não foi implementado.');
+    }
+
+    getSlashCommand(): SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder {
+        throw new Error(`o comando ${this.name} não foi implementado.`);
     }
 }
