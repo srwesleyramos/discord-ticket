d9ed642fdfe748abc8ad6e4a09acf0a46963ac7c
Wesley Ramos <dev.wesleygabriel@gmail.com>
2024-01-30 02:34:00 -0300

    feat: adding open ticket

--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -1,3 +1,9 @@
+import Sector from "./Sector";
+
+import {ChannelType, TextChannel} from "discord.js";
+
+import {v4 as uuid} from "uuid";
+
 export default class Ticket {
 
     readonly id: string;
@@ -6,10 +12,21 @@ export default class Ticket {
     sector_id: string | null;
     thread_id: string | null;
 
-    constructor(id: string, user_id: string, sector_id?: string, thread_id?: string) {
-        this.id = id;
+    constructor(id: string | null, user_id: string, sector_id?: string, thread_id?: string) {
+        this.id = id ?? uuid();
         this.sector_id = sector_id ?? null;
         this.thread_id = thread_id ?? null;
         this.user_id = user_id;
+    }
+
+    async open(sector: Sector, reason: string, parent: TextChannel) {
+        const thread = await parent.threads.create({
+            name: reason,
+            invitable: false,
+            type: ChannelType.PrivateThread
+        });
+
+        this.sector_id = sector.id;
+        this.thread_id = thread.id;
     }
 }
