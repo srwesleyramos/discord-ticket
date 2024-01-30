9217ea76718b1ad50f10fe37696b782cf42ea15a
Wesley Ramos <dev.wesleygabriel@gmail.com>
2024-01-30 02:46:00 -0300

    feat: adding welcome message

--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -1,6 +1,14 @@
 import Sector from "./Sector";
 
-import {ChannelType, TextChannel} from "discord.js";
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ChannelManager,
+    ChannelType,
+    EmbedBuilder,
+    TextChannel
+} from "discord.js";
 
 import {v4 as uuid} from "uuid";
 
@@ -28,5 +36,50 @@ export default class Ticket {
 
         this.sector_id = sector.id;
         this.thread_id = thread.id;
+
+        await this.welcomeMessage(sector, thread.client.channels);
+    }
+
+    async welcomeMessage(sector: Sector, channels: ChannelManager) {
+        if (!this.sector_id || !this.thread_id) return;
+
+        const generic = await channels.fetch(this.thread_id, {cache: true});
+        const channel = generic as TextChannel;
+
+        await channel.send({
+            components: [
+                new ActionRowBuilder<ButtonBuilder>()
+                    .addComponents(
+                        [
+                            new ButtonBuilder()
+                                .setCustomId("change-sector")
+                                .setLabel("Trocar setor")
+                                .setStyle(ButtonStyle.Primary),
+                            new ButtonBuilder()
+                                .setCustomId("close-ticket")
+                                .setLabel("Fechar atendimento")
+                                .setStyle(ButtonStyle.Danger)
+                        ] as any
+                    )
+            ],
+            embeds: [
+                new EmbedBuilder()
+                    .setAuthor({
+                        name: 'Informações sobre seu atendimento',
+                        iconURL: channel.client.user.avatarURL() ?? channel.client.user.defaultAvatarURL
+                    })
+                    .setDescription(
+                        'Olá! Bem-vindo ao nosso sistema de atendimento ao cliente!\n' +
+                        '\n' +
+                        'Informações:\n' +
+                        `<:right_arrow:975008491968290866> Atribuíção: <@&${this.sector_id}>\n` +
+                        `<:right_arrow:975008491968290866> Criado por: <@${this.user_id}>\n` +
+                        '\n' +
+                        'Horário comercial:\n' +
+                        '<:right_arrow:975008491968290866> segunda a sexta: 08:00 - 18:00\n' +
+                        '<:right_arrow:975008491968290866> sábado e domingo: 08:00 - 13:00'
+                    )
+            ]
+        });
     }
 }
