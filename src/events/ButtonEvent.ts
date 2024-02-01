fcf0237b31a7810dfdcdf0af48f970da4ad37566
Wesley Ramos <dev.wesleygabriel@gmail.com>
2024-01-31 22:57:06 -0300

    feat: adding close button

--- src/events/ButtonEvent.ts
+++ src/events/ButtonEvent.ts
@@ -1,5 +1,6 @@
 import Listener from "../interfaces/Listener";
 import Sectors from "../controllers/SectorController";
+import Tickets from "../controllers/TicketController";
 
 import {
     ActionRowBuilder,
@@ -17,29 +18,46 @@ export default class ButtonEvent extends Listener {
     }
 
     async execute(interaction: Interaction) {
-        if (!(interaction instanceof ButtonInteraction) || interaction.customId !== 'create-ticket') {
+        if (!(interaction instanceof ButtonInteraction)) {
             return
         }
 
-        const selectMenu = new StringSelectMenuBuilder()
-            .setCustomId(`create-ticket`)
-            .setPlaceholder('Selecione o setor adequado')
-            .addOptions(
-                Array.from(Sectors.cache.values())
-                    .map(sector =>
-                        new StringSelectMenuOptionBuilder()
-                            .setLabel(sector.label)
-                            .setDescription(sector.id)
-                            .setValue(sector.id)
-                    ) as any
-            );
+        if (interaction.customId === 'create-ticket') {
+            const selectMenu = new StringSelectMenuBuilder()
+                .setCustomId(`create-ticket`)
+                .setPlaceholder('Selecione o setor adequado')
+                .addOptions(
+                    Array.from(Sectors.cache.values())
+                        .map(sector =>
+                            new StringSelectMenuOptionBuilder()
+                                .setLabel(sector.label)
+                                .setDescription(sector.id)
+                                .setValue(sector.id)
+                        ) as any
+                );
 
-        await interaction.reply({
-            content: 'Qual o setor você quer abrir o atendimento?',
-            components: [
-                new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(selectMenu as any)
-            ],
-            ephemeral: true
-        });
+            await interaction.reply({
+                content: 'Qual o setor você quer abrir o atendimento?',
+                components: [
+                    new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(selectMenu as any)
+                ],
+                ephemeral: true
+            });
+        }
+
+        if (interaction.customId === 'close-ticket') {
+            const ticket = Tickets.getTicketByThread(interaction.channelId);
+
+            if (!ticket) {
+                return;
+            }
+
+            await interaction.reply({
+                content: 'O atendimento foi encerrado com sucesso.',
+                ephemeral: true
+            });
+
+            await ticket.close(interaction.client.channels, 'Atendimento finalizado com sucesso.');
+        }
     }
 }
